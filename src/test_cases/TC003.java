9600de367e0963248f9b9a97fcd079c10e9e3942
Yaroslav Surin <allstarcream@gmail.com>
2022-04-02 15:44:08 -0400

    Created TC003.

---
+++ src/test_cases/TC003.java
@@ -0,0 +1,16 @@
+package test_cases;
+
+public class TC003 {
+    public static void main(String[] args) {
+
+
+        // Test case TC003 created.
+
+        // First lines is here.
+
+
+
+
+
+    }
+}
